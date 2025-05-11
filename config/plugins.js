b1c427d08eef683e4ca67c1584e995d99e5d20d4
sherylchan <sheryl@blackandweb.com>
2025-05-12 01:26:24 +0800

    Install AWS S3 provider for R2

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,21 @@
 module.exports = ({ env }) => ({
   "users-permissions": {
     config: {
-      jwtSecret: env("JWT_SECRET"),
+      jwtSecret: env("a5vj98bm4h0ik3ufgtbpgb7hwqe2poc1"),
     },
   },
   upload: {
     config: {
-      provider: "local",
-      actionOptions: {
-        upload: {},
-        delete: {},
+      provider: "aws-s3",
+      providerOptions: {
+        accessKeyId: env('71484efb957958c9916be1ba53377e33'),
+        secretAccessKey: env('00507372d32b8dcbffd0dfb095c2bf003b251895d2cda563bea2b8324aa02fb4'),
+        endpoint: env('https://2e800734092d7b1506bc181e26e17348.r2.cloudflarestorage.com/acv-strapi-media'),
+        region: 'auto',
+        params: {
+          Bucket: env('acv-strapi-media'),
+        },
+        s3ForcePathStyle: true,
       },
     },
   },
